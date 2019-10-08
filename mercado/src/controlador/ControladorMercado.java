abbb03536b65ff466bae99b555f1360ba305156d
Administrador <Administrador@10.10.4.19>
2019-10-08 16:47:00 -0300

    3

---
+++ mercado/src/controlador/ControladorMercado.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import tela.manutencao.ManutencaoMercado;
+import dao.DaoMercado;
+import javax.swing.JOptionPane;
+import modelo.Mercado;
+import  java.time.LocalDate; 
+import java.time.format.DateTimeFormatter;
+/**
+ *
+ * @author Administrador
+ */
+public class ControladorMercado {
+
+    public static void inserir(ManutencaoMercado man){
+        Mercado objeto = new Mercado();
+        objeto.setCodigo(Integer.parseInt(man.jtfCodigo.getText()));
+        objeto.setNome_fantasia(man.jtfNome_fantasia.getText());
+        objeto.setRazao_social(man.jtfRazao_social.getText());
+        objeto.setFundacao(LocalDate.parse(man.jtfFundacao.getText(), DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        objeto.setNr_funcionario(Integer.parseInt(man.jtfNr_funcionario.getText()));
+        objeto.setValor_bolsa(Double.parseDouble(man.jtfValor_bolsa.getText()));
+        boolean resultado = DaoMercado.inserir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+}
+    
+}
---
+++ mercado/src/dao/DaoMercado.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import modelo.Mercado;
+import java.sql.Date;
+import  java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+/**
+ *
+ * @author Administrador
+ */
+public class DaoMercado {
+    public static boolean inserir(Mercado objeto) {
+        String sql = "INSERT INTO mercado (codigo, nome_fantasia, razao_social, fundacao, nr_funcionario, valor_bolsa) VALUES (?, ?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setInt(1, objeto.getCodigo());
+            ps.setString(2, objeto.getNome_fantasia());
+            ps.setString(3, objeto.getRazao_social());
+            ps.setDate(4, Date.valueOf(objeto.getFundacao()));
+            ps.setInt(5, objeto.getNr_funcionario());
+            ps.setDouble(6, objeto.getValor_bolsa());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+     public static void main(String[] args) {
+        Mercado objeto = new Mercado();
+        objeto.setCodigo(1);
+        objeto.setNome_fantasia("aaaa");
+        objeto.setRazao_social("aaaa");
+        objeto.setFundacao(LocalDate.parse("11/01/1988", DateTimeFormatter.ofPattern("dd/MM/yyyy"))); 
+        objeto.setNr_funcionario(30);
+        objeto.setValor_bolsa(225.50);
+        
+        boolean resultado = inserir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+}
